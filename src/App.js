a3ae24908cdde1c6538f760c1b673f41650b12b1
mustkeemk <mustkeem@sparxitsolutions.com>
2018-10-30 18:27:45 +0530

    add utility

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import ContactData from './components/ContactData';
 
@@ -8,12 +7,12 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+         
+          <h1 className="App-title">Demo Form</h1>
         </header>
-        <p className="App-intro">
+        
           <ContactData />
-        </p>
+       
       </div>
     );
   }
---
+++ src/components/FormGenerator.js
@@ -0,0 +1,71 @@
+
+import React from 'react';
+import Input from './Input';
+import checkValidity from './Utility'
+
+class FormGenerator extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            orderForm:this.props.formConfig,
+            formIsValid: false,
+            loading: false
+        }
+    }
+
+    inputChangedHandler = (event, inputIdentifier) => {
+        const updatedOrderForm = {
+            ...this.state.orderForm
+        };
+        const updatedFormElement = {
+            ...updatedOrderForm[inputIdentifier]
+        };
+        updatedFormElement.value = event.target.value;
+        updatedFormElement.valid = checkValidity(updatedFormElement.value, updatedFormElement.validation);
+        updatedFormElement.touched = true;
+        updatedOrderForm[inputIdentifier] = updatedFormElement;
+
+        let formIsValid = true;
+        for (let inputIdentifier in updatedOrderForm) {
+            formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
+        }
+        this.setState({ orderForm: updatedOrderForm, formIsValid: formIsValid });
+    }
+
+    submitHandler = (event) => {
+        event.preventDefault();
+        this.setState({ loading: true });
+       console.log(this.state.orderForm);
+    }
+
+    render() {
+        const formElementsArray = [];
+        for (let key in this.state.orderForm) {
+            formElementsArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            });
+        }
+
+        let form = (
+            <form onSubmit={this.submitHandler}>
+                {formElementsArray.map(formElement => (
+                    <Input 
+                        key={formElement.id}
+                        elementType={formElement.config.elementType}
+                        elementConfig={formElement.config.elementConfig}
+                        value={formElement.config.value}
+                        invalid={!formElement.config.valid}
+                        shouldValidate={formElement.config.validation}
+                        touched={formElement.config.touched}
+                        changed={(event) => this.inputChangedHandler(event, formElement.id)} />
+                ))}
+                <button disabled={!this.state.formIsValid}>Submit</button>
+            </form>
+        );  
+        return (
+            <div>{form}</div>
+        )
+    }
+}
+export default FormGenerator;
